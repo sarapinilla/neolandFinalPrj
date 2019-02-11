8612ee406d4ea38da29f972c13b803d0f5904781
Sara Pinilla <sarapini19@gmail.com>
2019-02-11 17:09:04 +0100

    rutas/componentes a crear comentadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //header,main,footer /select-about,comofunciona,quehacemos,galeria,descarga /fichaArtista,pieza /FUTURO: login,eventos,calendario
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
