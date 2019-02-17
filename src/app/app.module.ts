11804609089c150c97a2eebe76175905f5265ddc
Sara Pinilla <sarapini19@gmail.com>
2019-02-17 16:41:54 +0100

    Inicio About(Home)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
