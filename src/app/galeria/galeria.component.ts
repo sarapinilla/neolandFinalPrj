7aa75db5409cdd882f14263ba37d38bb3d5682d3
Sara Pinilla <sarapini19@gmail.com>
2019-02-19 20:48:05 +0100

    Creación de componente Galería, los modelos Autor y Pieza, y los servicios AutoresService y PiezasService. Maquetación provisional y avances en Galería.

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Autor } from '../models/autor.model';
+import { AutoresService } from '../autores.service';
+import { PiezasService } from '../piezas.service';
 
 @Component({
   selector: 'galeria',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleriaComponent implements OnInit {
 
-  constructor() { }
+  autores: Autor[]
+  piezas: Pieza[]
+
+  constructor(private autoresService: AutoresService, private piezasService: PiezasService) { 
+    // this.autoresService.getAllAutores().subscribe(res =>{
+    //   this.autores = res
+    // })
+    this.piezasService.getAllPiezas().subscribe(res => {
+      this.piezas = res
+    })
+  }
 
   ngOnInit() {
   }
+
+// filtrar($event){
+
+// }
+
 
 }
