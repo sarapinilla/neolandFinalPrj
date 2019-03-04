fe8a82163ee44d5123093df9348aafdd1866d014
anasfresneda <anasfresneda@gmail.com>
2019-03-04 23:05:04 +0100

    [RUTAS] routing.ts + router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//Components importados:
+import { MainComponent } from './main/main.component';
+import { FormComponent } from './form/form.component';
+import { FichaComponent } from './ficha/ficha.component';
+import { AboutComponent } from './about/about.component';
+import { FuncionamientoComponent } from './funcionamiento/funcionamiento.component';
+import { DownloadComponent } from './download/download.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 const routes: Routes = [
-  //header,main,footer /select-about,comofunciona,quehacemos,galeria,descarga /fichaArtista,pieza /FUTURO: login,eventos,calendario
+  { path: '', redirectTo: 'home', pathMatch:'full'},
+  { path: 'home', component: MainComponent, children:[
+    { path: 'home/about', component: AboutComponent},
+    { path: 'home/funcionamiento', component: FuncionamientoComponent},
+    { path: 'home/galeria', component: GaleriaComponent}, //TODO > :id
+    { path: 'home/app', component:DownloadComponent}
+  ]},
+  //{ path: 'contact', component: ContactComponent}, //TO DO: contactComponent ---> wanna know more?
+  { path: 'registro', component: FormComponent},
+  { path: 'ficha', component: FichaComponent},
+
+  // { path: '/admin', component: loginComponent}, //TODO: only admin users
+  { path: '**', component: MainComponent }
 ];
 
 @NgModule({
