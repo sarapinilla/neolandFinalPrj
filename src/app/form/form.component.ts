091d93c6a9d68119c36039bc0c456c1fd542dba0
anasfresneda <anasfresneda@gmail.com>
2019-03-07 17:22:29 +0100

    [FORM] Inicio validaciones

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators'
 
 @Component({
   selector: 'app-form',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+formRegistro: FormGroup
+
   constructor() { }
 
   ngOnInit() {
+    this.formRegistro = new FormGroup({
+      
+      tituloPieza: new FormControl('Título pieza', [
+        Validators.required,
+        Validators.maxLength(50)
+      ]),
+      infoPieza: new FormControl('Informaciónpieza',[
+        Validators.required,
+        Validators.maxLength(200)
+      ]),
+      picCover: new FormControl('Foto Cover', [
+        //Investigar
+      ]),
+      picPieza: new FormControl('Archivo Pieza', [
+        //Investigar
+      ]),
+      //aqui CATEGORIAS
+
+      nomnbreAutor: new FormControl('Nombre Autor', [
+        Validators.required,
+        Validators.maxLength(50)
+      ]),
+
+
+  
+      
+      
+
+
+
+
+
+    })
+
+    let tituloPiezaControl = this.formRegistro.controls.tituloPieza
+    //valueChanges -> observable , por eso le ponemos .subscribe
+    tituloPiezaControl.valueChanges.pipe(debounceTime(500)).subscribe((value)=>{
+      console.log(value)
+    })
+
+  }
+  //Reset valores cuando se envía el formulario:
+  manejarFormulario(){
+    console.log(this.formRegistro.value)
+    this.formRegistro.reset()
   }
 
 }
