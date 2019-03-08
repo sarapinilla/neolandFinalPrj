5541e72c897938f7c902cbb0fe00c587cb9da644
Sara Pinilla <sarapini19@gmail.com>
2019-03-08 10:44:10 +0100

    Conexión con la DB en el servicio de Autores. Acceso a los autores según su id en ficha.ts Instalación de Fonts Awesome

--- src/app/ficha/ficha.component.ts
+++ src/app/ficha/ficha.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AutoresService } from '../autores.service';
 
 @Component({
   selector: 'ficha',
@@ -9,14 +10,27 @@ import { ActivatedRoute } from '@angular/router';
 export class FichaComponent implements OnInit {
 
   autor: any
+  autorId: any
+  autorImg: string
+  piezaImg: string[]
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private autoresService: AutoresService
+    ) {
     this.activatedRoute.params.subscribe((params) => {
-        this.autor = params.autorId
+      this.autorId = params.autorId     
     })
+    
+    this.autoresService.getAutoresById(this.autorId).subscribe(res =>{
+        this.autor = res
+        console.log(this.autor)   
+    })   
+
    }
 
-  ngOnInit() {
+  ngOnInit() { 
+    // document.getElementById('a').style.backgroundImage = `url(${this.autor.imgautor})`
   }
 
 }
